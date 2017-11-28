f63c3bb6133b31d80153934a945f9bb51abdfeb7
Vincent S. Cojot <vcojot@redhat.com>
2017-11-28 18:33:18 -0500

    General bugfixes

--- pull_playbooks.sh
+++ pull_playbooks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avcP --exclude=update.sh --exclude=pull_playbooks.sh --exclude=push_all.sh --exclude=push_playbooks.sh root@198.154.189.185:/root/ansible/ .
+rsync -avcP --exclude=.git --exclude=update.sh --exclude=pull_playbooks.sh --exclude=push_all.sh --exclude=push_playbooks.sh root@198.154.189.185:/root/ansible/ .
 
