c0af24b699434175c1d15a6eaa3f900e2d4f2026
Vincent S. Cojot <vincent@cojot.name>
2018-02-21 10:05:58 -0500

    Fixes for root

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for vm in $(virsh list |awk '{ if ( $1 ~ /[0-9]/ ) { print $2 } }')
+for vm in $(sudo virsh list |awk '{ if ( $1 ~ /[0-9]/ ) { print $2 } }')
 do
-	virsh destroy $vm
+	sudo virsh destroy $vm
 done
 
-for vm in $(virsh list --all |awk '{ if ( $1 == "-" ) { print $2 } }')
+for vm in $(sudo virsh list --all |awk '{ if ( $1 == "-" ) { print $2 } }')
 do
-	virsh undefine $vm
-	/bin/rm -rfv /var/lib/libvirt/images/${vm}*.qcow2
+	sudo virsh undefine $vm
+	sudo /bin/rm -rfv /var/lib/libvirt/images/${vm}*.qcow2
 done
