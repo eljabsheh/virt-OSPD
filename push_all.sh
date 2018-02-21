118c4aca970a08f7f0091dd3e4719300d332bad4
Vincent S. Cojot <vincent@cojot.name>
2018-02-21 18:02:22 -0500

    Updates

--- push_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for myip in $(cat floatings.txt)
-do
-	ssh-copy-id root@${myip}
-	rsync -avcP --delete --exclude=update.sh --exclude=pull_playbooks.sh --exclude=push_all.sh --exclude=push_playbooks.sh . root@${myip}:/root/ansible
-done
-
