0ce8b7242905a85fae1fab2a09593dee6e01f5a2
Vincent S. Cojot <vincent@cojot.name>
2018-02-22 17:24:01 -0500

    Exclude instack from cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,12 +4,12 @@ if [ $UID -ne 0 ]; then
         echo "Run $0 as root!" ; exit 127
 fi
 
-for vm in $(virsh list |awk '{ if ( $1 ~ /[0-9]/ ) { print $2 } }')
+for vm in $(virsh list|grep -vw instack |awk '{ if ( $1 ~ /[0-9]/ ) { print $2 } }')
 do
 	virsh destroy $vm
 done
 
-for vm in $(virsh list --all |awk '{ if ( $1 == "-" ) { print $2 } }')
+for vm in $(virsh list --all|grep -vw instack |awk '{ if ( $1 == "-" ) { print $2 } }')
 do
 	virsh undefine $vm
 	/bin/rm -fv /var/lib/libvirt/images/${vm}*.qcow2
