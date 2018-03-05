c34108990ac655fbea517350f65e11eddaffa70c
Vincent S. Cojot <vincent@cojot.name>
2018-03-05 18:56:10 -0500

    First working implementation of vbmc setup driven through ansible

--- runme.sh
+++ runme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
-ansible-playbook -i inventories/virt-env-ospd/hosts  playbooks/virt-env-ospd/ospd.yml
+ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml
 
-
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-virtualbmc
 # ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-prepare
-#  ansible-playbook -i inventories/virt-env-ospd/hosts  playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-hypervisor,virt-env-ospd-undercloud,virt-env-ospd-templates
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-hypervisor,virt-env-ospd-undercloud,virt-env-ospd-templates
 
