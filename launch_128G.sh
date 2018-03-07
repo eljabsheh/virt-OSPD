a44eade91377a255226c232663d9156033164f25
Vincent S. Cojot <vincent@cojot.name>
2018-03-07 12:00:00 -0500

    provide several profiles

---
+++ launch_128G.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd_128G.yml
+
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-virtualbmc
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-prepare
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-hypervisor,virt-env-ospd-undercloud,virt-env-ospd-templates
+
--- push_playbooks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync -avcP . root@198.154.189.185:/root/ansible
-
