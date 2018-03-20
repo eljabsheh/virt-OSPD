6ab10d035960f05701dd64176cd4fa9eeef24b84
Vincent S. Cojot <vincent@cojot.name>
2018-03-20 15:45:11 -0400

    Update to roles, add 12-director and fixe pm_type issue

--- launch_64G.sh
+++ launch_64G.sh
@@ -2,7 +2,7 @@
 set -x
 ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd_64G.yml
 
-# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-virtualbmc
-# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-prepare
-# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd.yml --tags virt-env-ospd-hypervisor,virt-env-ospd-undercloud,virt-env-ospd-templates
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd_64G.yml --tags virt-env-ospd-virtualbmc
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd_64G.yml --tags virt-env-ospd-prepare
+# ansible-playbook -i inventories/virt-env-ospd/hosts playbooks/virt-env-ospd/ospd_64G.yml --tags virt-env-ospd-hypervisor,virt-env-ospd-undercloud,virt-env-ospd-templates
 
