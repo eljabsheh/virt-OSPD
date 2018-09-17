10a0109a9c43460127d2170e23bb48edc30c00ff
Vincent S. Cojot <vincent@cojot.name>
2018-09-17 14:41:29 -0400

    Updates

--- launch.sh
+++ launch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Give hints:
+echo -e "(II) Here are some suggestions for some specific systems:"
+echo -e "----------------------------------------------"
+echo -e " - p50r or ravenvale\t: 32G config"
+echo -e " - daltigoth\t\t: 64G config"
+echo -e " - palanthas\t\t: 160G config"
+echo -e " - thorbardin\t\t: 192G config"
+echo -e " - vkvm1\t\t: 256G config"
+echo -e "----------------------------------------------\n"
+
 # Select OSP version..
 PS3="(II) Please select an OSP version: "
 select option in 8 9 10 11 12 13
@@ -17,10 +27,10 @@ done
 
 # Select RAM
 PS3="(II) Please select memory config for your hypervisor: "
-select option in 32G 64G 128G 192G 256G
+select option in 32G 64G 128G 160G 192G 256G
 do
 	case ${option} in
-		32G|64G|128G|192G|256G)
+		32G|64G|128G|160G|192G|256G)
 			RAM=${option}
 			break
 			;;
